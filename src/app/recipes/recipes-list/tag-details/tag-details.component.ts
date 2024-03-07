1c3f26068d0fdb2c40bf969040b1d04a76ac182e
Pawel W <pswegr@hotmail.com>
2024-03-08 00:58:05 +0100

    Add title and description tag on details

--- src/app/recipes/recipes-list/tag-details/tag-details.component.ts
+++ src/app/recipes/recipes-list/tag-details/tag-details.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { filter, map, share, switchMap } from 'rxjs';
+import { filter, map, share, switchMap, tap } from 'rxjs';
 import { RecipesService } from '../../../shared/services/recipes.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tag-details',
@@ -11,9 +12,13 @@ import { RecipesService } from '../../../shared/services/recipes.service';
 export class TagDetailsComponent {
   tagName$ = this.route.params.pipe(filter(p => p['tag'] !== null), map(p => p['tag']));
   recipesByTag$ = this.tagName$.pipe(
+    tap(tag => {
+      this.titleService.setTitle(`${tag} - Recipes with Passion / ${tag} przepisy`);
+      this.metaService.updateTag({name: 'decription', content: `Culinary recipes with tag: ${tag} / Przepisy kulinarne otagowane: ${tag}`})
+    }),
     switchMap(tag => this.recipesService.getRecipes([tag])),
   ).pipe(share());
 
-  constructor(private route: ActivatedRoute, private recipesService: RecipesService ) { }
+  constructor(private route: ActivatedRoute, private recipesService: RecipesService, private titleService: Title, private metaService: Meta ) { }
 
 }
