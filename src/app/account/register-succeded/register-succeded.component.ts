89c9ed62c93bab5ad53c5846e3cd39eacee060b6
Paweł Węgrzyniak <pswegr@gmail.com>
2024-07-08 18:36:07 +0200

    Improve registration flow

---
+++ src/app/account/register-succeded/register-succeded.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-succeded',
+  templateUrl: './register-succeded.component.html',
+  styleUrl: './register-succeded.component.scss'
+})
+export class RegisterSuccededComponent {
+
+}
