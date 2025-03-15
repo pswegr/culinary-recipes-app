2da3cb8eeb75ac3e7a32b44937d1a7f79309566b
Paweł Węgrzyniak <pswegr@gmail.com>
2025-03-15 01:21:16 +0100

    loading search overlay items animation

--- src/app/shared/services/loading.service.ts
+++ src/app/shared/services/loading.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, debounceTime, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
-
   loadingSubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.loadingSubject.asObservable();
 
   loadingOn() {
     this.loadingSubject.next(true);
